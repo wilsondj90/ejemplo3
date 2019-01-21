f2111961a73399413a2b8f9ff22a509b42f91b49
Wilson De Jesús Correa Berrio <Wcorrea@intergrupo.com>
2019-01-21 14:50:33 -0500

    se crea otro ejemplo para el repositorio

---
+++ ejemplo2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
